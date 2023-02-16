df9d154f4c0b9d27eef7a8319b0156f9a31a1924
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-16 12:07:24 -0800

    5.Hello_printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+nclude <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
