354eee174a8721621fb5898a6194d7e5758e28b8
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-17 16:19:02 -0800

    0.Positive_anything_is_better_than_negative_nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,13 @@
-/*
- *File: 0-positive_or_negative.c
- * Auth: Aya Ashraf
- */
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -22,5 +19,6 @@ int main (void)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	return(0);
+	return (0);
 }
+
