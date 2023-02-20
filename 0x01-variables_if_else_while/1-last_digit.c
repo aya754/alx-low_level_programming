4ba89942116b2bf934c3fcbb9bc5099180e7ed94
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-19 16:05:26 -0800

    1.The_last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - prints the last digit of the random
- * number stored in the variable n
+ * Description: 'number stored in the variable n'
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
