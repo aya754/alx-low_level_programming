24ab6da13ef375e55fe328bf521f167595a0a2d9
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-19 16:31:40 -0800

    4.When_I_was_having_that_alphabet_soup_I_never_thought_that_it_would_pay_off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Description: 'except q and e & followed by a new line'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
