8857ce07fa00c04c80c4eb2ab9c7a7f68efe9f88
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-23 16:05:17 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,22 +9,43 @@
 int main(void)
 {
 	int i;
-
 	unsigned long fibo1 = 0, fibo2 = 1, sum;
+	unsigned long fibo1_half1, fibo1_half2, fib02_half1, fib02_half2;
+	unsigned long half1, half2;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		sum = fibo1 + fibo2;
-		printf("%lu", sum);
+		printf("%lu, ", sum);
 
 		fibo1 = fibo2;
 		fibo2 = sum;
-
-		if (i == 97)
-			printf("\n");
-		else
-			printf(", ");
 	}
 
+	fibo1_half1 = fibo1 / 10000000000;
+	fibo2_half1 = fibo2 / 10000000000;
+	fibo1_half2 = fibo1 % 10000000000;
+	fibo2_half2 = fibo2 % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		half1 = fibo1_half1 + fibo2_half1;
+		half2 = fibo1_half2 + fibo2_half2;
+		if (fibo1_half2 + fibo2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (i != 98)
+			printf(", ");
+
+		fibo1_half1 = fibo2_half1;
+		fibo1_half2 = fibo2_half2;
+		fibo2_half1 = half1;
+		fibo2_half2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
