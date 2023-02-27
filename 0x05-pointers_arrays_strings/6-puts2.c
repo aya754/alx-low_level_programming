eea9cc561e2f398de86ffdb9d15f1d44e7df29b1
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-27 15:40:03 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * puts2 - prints every other character of a string
  *
@@ -21,5 +21,6 @@ void puts2(char *str)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
