3dd9f49f120fd7dac164e1908ca99ba8aa215fe7
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-02-27 14:44:09 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @str: The string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (r -= 1; r >= 0; r--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
