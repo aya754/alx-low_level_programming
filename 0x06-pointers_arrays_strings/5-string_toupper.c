e5fe6e22ceaf0b14da63cbdde54250511dbffa05
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-03-03 08:44:00 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @s: the string be changed
+ *
+ * Return: the string that change for uppercase
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+			i++;
+		}
+	}
+	return (s);
+}
