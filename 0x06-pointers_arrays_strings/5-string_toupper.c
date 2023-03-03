e8b16132e5c73d1392367764c4677f9fac8b87a2
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-03-03 08:52:08 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,14 +11,11 @@ char *string_toupper(char *)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 97 && s[i] <= 122)
-		{
-			`s[i] = s[i] - 32;
-			i++;
-		}
+			s[i] = s[i] - 32;
+		i++;
 	}
 	return (s);
 }
