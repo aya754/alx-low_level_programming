1ba29fa1dc2a0fe36592f0122a406b74255bccdb
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-03-04 10:57:53 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the number be printed
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int n;
+
+	_putchar("%i\n", n);
+
+	return (0);
+}
+
