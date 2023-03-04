85f6b1b29e91f37e3a1cc3eed919efba743d98bd
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-03-03 16:39:33 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @n: the input string
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i, k;
+
+	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; x[k] != '\0'; k++)
+		{
+			if (s[i] == x[k])
+			{
+				s[i] = y[k];
+				break;
+			}
+		}
+	}
+	return (s);
+}
