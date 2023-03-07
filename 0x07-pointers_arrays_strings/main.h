3fb8f8321e5c5a930030cebc560ea199a931e05e
Aya Ashraf <ayaashibrahim765@gmail.com>
2023-03-07 15:26:20 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER-FILE
 #define HEADER-FILE
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
